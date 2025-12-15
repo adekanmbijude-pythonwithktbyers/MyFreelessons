70d401668ca786d51a08723632fcebc1d78305a2
Olasunkanmi Adekanmbi <king-sneh@Olasunkanmis-MacBook-Pro-2.local>
2025-12-14 20:34:43 -0700

    my ex5

---
+++ ex5.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+ip_addr = "10.12.17.1"
+mac_addr = "0024.c4e9.48ae"
+information = ip_addr +  ' -->' + mac_addr
+print(information)
+
+print(f"{ip_addr} --> {mac_addr}")
+
+print("{} --> {}".format(ip_addr, mac_addr) )
