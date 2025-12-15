4f9a1538a4edf04d19ca8ceb3598b8672ffcea4a
Olasunkanmi Adekanmbi <king-sneh@Olasunkanmis-MacBook-Pro-2.local>
2025-12-14 19:27:25 -0700

    Exercise1

---
+++ ex1.py
@@ -0,0 +1,7 @@
+
+#!/usr/bin/env python
+sf_gw1 = "172.31.255.1/24"
+sf_gw2 = input("Please Input the correct IP address: ")
+print(f"{'sf_gw1':^20} {'sf_gw2':^20}")
+print(f"{'-'* 20:^20} {'-'* 20:^20}")
+print(f"{sf_gw1:^20} {sf_gw2:^20}")
